46da415f9d8974c0a0703a630276710b959f0893
jleilani <jxleilani@gmail.com>
2020-12-01 15:03:55 -0500

    started adding routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
+const db = require('./models');
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,8 +20,29 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useFindAndModify: false
 });
 
-//Routes
+//HTML Routes
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
 
+//API Routes
+app.get('/api/workouts', (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkout => res.json(dbWorkout))
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+app.post('/api/workouts/', (req, res) => {
+  db.Workout.create(req.body)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
 
 //Server listening
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
